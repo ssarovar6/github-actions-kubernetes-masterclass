a624d3eafae1836f32334b603cc1402dec59bda4
ssarovar6 <root@yeevashi.localdomain>
2026-05-25 10:08:15 +0530

    added kube-state-metrics and node exporter

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -74,6 +74,14 @@ resource "aws_security_group" "allow_user_to_connect" {
 
   ingress {
     description = "port 8888 allow"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "port 8888 allow"
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
